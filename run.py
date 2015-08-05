12a3ded64cc34596f1c279537efa522585cffe6e
Sam Clift <sam@cloudfindhq.com>
2015-08-05 21:19:41 +0100

    page and retweet improve alg

--- run.py
+++ run.py
@@ -9,11 +9,26 @@ api = twitter.Api(consumer_key=creds['consumer_key'],
                   access_token_key=creds['access_token_key'],
                   access_token_secret=creds['access_token_secret'])
 
-search_results = api.GetSearch('retweet to win! -follow -tune -email -link', count=100)
 
-for result in search_results:
-    try:
-        api.PostRetweet(result.GetId())
-        print 'retweeted ', result.text
-    except twitter.error.TwitterError:
-        print 'already retweeted ', result.text
+def page_and_retweet(page=None):
+    search_results = api.GetSearch('retweet win -follow -tune -email -link -fav -vote',
+                                   count=100,
+                                   since_id=page)
+
+    for result in search_results:
+        try:
+            api.PostRetweet(result.GetId())
+            print 'retweeted ', result.text
+        except twitter.error.TwitterError:
+            print 'duplicate retweets'
+            pass
+
+    # return the last id
+    return search_results[-1].id
+
+last_id = page_and_retweet()
+last_id = page_and_retweet(last_id)
+last_id = page_and_retweet(last_id)
+last_id = page_and_retweet(last_id)
+last_id = page_and_retweet(last_id)
+last_id = page_and_retweet(last_id)
